12f3e851ff235b71792dde17df67d441854e38d3
Carlos Richard <100031527+Crichard7@users.noreply.github.com>
2023-01-09 23:46:30 -0300

    Teste_bst

---
+++ teste_bst.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include "bst.h"
+
+int main(){
+    no *busca, *raiz = NULL;
+    int opcao, valor;
+
+    do{
+        printf("\nArvore BST\n1- Inserir\n2 - Imprimir em ordem\n3 - Buscar valor\n0 - Sair");
+        printf("\nDigite uma opcao: \n");
+        scanf("%d", opcao);
+        switch(opcao){
+            case 1:
+                //Inserir
+                printf("\nDigite o valor a ser inserido: \n");
+                scanf("%d", &valor);
+                raiz = inserir(raiz, valor);
+                break;
+            case 2:
+                //Imprimir em Ordem
+                inOrder(raiz);
+                break;
+            case 3:
+                //Buscar
+                printf("\nDigite o valor que ser procurado: \n");
+                scanf("%d", valor);
+                busca = buscar(raiz, valor);
+                if(busca != NULL){
+                    printf("\nValor encontrado: %d\n", busca->valor);
+                }
+                else{
+                    printf("\nValor nao encontrado\n");
+                }
+                break;
+            default:
+                if(opcao != 0);
+                printf("\nOpcao Invalida!!!\n");
+        }
+    }while(opcao != 0);
+}
